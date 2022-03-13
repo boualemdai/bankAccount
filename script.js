f50604de50c6027c59cfe0756d4c3fc4f7e62e76
boualemdai <boualemdai@gmail.com>
2022-03-13 11:24:11 +0100

    bankAccount challenge

--- script.js
+++ script.js
@@ -18,18 +18,65 @@ const out = document.querySelector("#out>span");
 const interest = document.querySelector("#interest>span");
 const sort = document.getElementById("sort");
 const chrono = document.querySelector(".chrono>span");
-console.log(chrono);
-let seconds = 0;
-let mins = 3;
+const greeting = document.querySelector("header>h2");
 
-const initialArr = [1000, 5000, 5000, -800, -1200, -1000];
-const arr = [];
+let seconds;
+let mins;
 let sorted = false;
-initialArr.map((e) => soldefn(e));
 
-function soldefn(val) {
-  arr.push(val);
+const userObj = {
+  boualem: "123",
+  mouloud: "456",
+};
+const obj = {
+  boualem: [1000, 5000, 5000, -800, -1200, -1000],
+  mouloud: [-2000, 5500, 6220, -950, 3000, -478],
+};
+
+const initialState = () => {
+  logoutName.value = null;
+  logoutPassword.value = null;
+  solde.textContent = null;
+  greeting.textContent = "Log in to get started";
+  tableau.textContent = null;
+  transferTo.value = null;
+  transfer.value = null;
+  loan.value = null;
+  userName.value = null;
+  passworld.value = null;
+  main.classList.add("hidden");
+};
+initialState();
+
+//check user
+
+function checkeUser(user, pin) {
+  for (const [key, value] of Object.entries(userObj)) {
+    console.log(key);
+    if (user === key && pin === value) {
+      relevet(key);
+    }
+  }
+}
+
+// map relevet
+
+function relevet(user) {
+  tableau.innerHTML = null;
+  solde.textContent = null;
+  obj[user].map((e) => soldefn(e, user));
+  main.classList.remove("hidden");
+
+  mins = 10;
+  seconds = 0;
+  setInterval(chronofn, 1000);
+}
+
+// display
+
+function soldefn(val, user) {
   solde.textContent = (Number(solde.textContent) + val).toFixed(2);
+
   tableau.innerHTML += `
             <div class="exemple">
                 <p>${val > 0 ? "Deposit" : "Withdrew"}</p>
@@ -37,17 +84,23 @@ function soldefn(val) {
                 <p>${val}</p>
             </div>
                     `;
-  const newArr = arr.reduce(
+  let newArr = [];
+
+  newArr = obj[user].reduce(
     ([positive, neagative], val) => {
       val > 0 ? (positive += val) : (neagative += val);
       return [positive, neagative];
     },
     [0, 0]
   );
+
   iN.textContent = newArr[0];
   out.textContent = newArr[1];
-  interest.textContent = newArr[0] * 0.01;
+  interest.textContent = (newArr[0] * 0.01).toFixed(2);
 }
+
+//chrono
+
 const chronofn = () => {
   if (mins !== 0) {
     if (seconds === 0) {
@@ -57,44 +110,58 @@ const chronofn = () => {
   } else if (seconds !== 0) {
     seconds--;
   } else {
-    main.classList.add("hidden");
+    initialState();
   }
   chrono.textContent = `${mins > 9 ? mins : "0" + mins}:${
     seconds > 9 ? seconds : "0" + seconds
   }`;
 };
 
-currentDate.textContent = new Date().toLocaleDateString("FR-fr");
-logIn.addEventListener("click", () => {
-  if (userName.value === "boualem" && passworld.value === "123") {
-    main.classList.remove("hidden");
+//log in
 
-    mins = 10;
-    seconds = 0;
-    setInterval(chronofn, 1000);
-  }
+logIn.addEventListener("click", () => {
+  checkeUser(userName.value, passworld.value);
+  greeting.textContent = `Hello Mr: ${userName.value}`;
 });
+
+//loan
+
 loanBtn.addEventListener("click", () => {
   const loanValue = Number(loan.value);
-  soldefn(loanValue);
+  obj[userName.value].push(loanValue);
+  soldefn(loanValue, userName.value);
+  loan.value = null;
 });
+
+// log out
+
 logoutBtn.addEventListener("click", () => {
-  if (logoutName.value === "boualem" && logoutPassword.value === "123") {
-    main.classList.add("hidden");
+  if (
+    logoutName.value === userName.value &&
+    logoutPassword.value === passworld.value
+  ) {
+    initialState();
   }
-  logoutName.value = null;
-  logoutPassword.value = null;
 });
+
+// transfer
 
 transferBtn.addEventListener("click", () => {
   const transferValue = Number(transfer.value);
-  soldefn(-transferValue, "Withdrew");
+  obj[userName.value].push(-transferValue);
+  obj[transferTo.value].push(transferValue);
+  soldefn(-transferValue, userName.value);
+  transfer.value = null;
+  transferTo.value = null;
 });
+
+//sort
+
 sort.addEventListener("click", () => {
   tableau.innerHTML = null;
   console.log(sorted);
   if (!sorted) {
-    let sortedArr = [...arr];
+    let sortedArr = [...obj[userName.value]];
 
     sortedArr
       .sort((a, b) => b - a)
@@ -109,7 +176,7 @@ sort.addEventListener("click", () => {
       });
     sorted = true;
   } else {
-    arr.map((e) => {
+    obj[userName.value].map((e) => {
       tableau.innerHTML += `
           <div class="exemple">
               <p>${e > 0 ? "Deposit" : "Withdrew"}</p>
@@ -121,3 +188,5 @@ sort.addEventListener("click", () => {
     sorted = false;
   }
 });
+
+currentDate.textContent = new Date().toLocaleDateString("FR-fr");
